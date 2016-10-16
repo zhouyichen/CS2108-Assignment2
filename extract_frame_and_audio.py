45b776261703c3dd2b5a7d9c0504b099dc54215a
zhouyichen <zhouyichen123@gmail.com>
2016-10-16 23:29:45 +0800

    improve extraction to ignore errors

--- extract_frame_and_audio.py
+++ extract_frame_and_audio.py
@@ -9,7 +9,7 @@ def getKeyFrames(vidcap, store_frame_path):
     sumDiff = []
     frames = []
 
-    while vidcap.isOpened():
+    while True:
         success, frame = vidcap.read()
         if not success:
             break
@@ -24,6 +24,9 @@ def getKeyFrames(vidcap, store_frame_path):
         lastHist = hist
         count += 1
 
+    if len(frames) == 0:
+        return
+
     m = np.mean(sumDiff)
     std = np.std(sumDiff)
 
@@ -31,7 +34,7 @@ def getKeyFrames(vidcap, store_frame_path):
     candidates_value = []
     for i in range(len(sumDiff)):
         if sumDiff[i] > m + std*3:
-            candidates.append(i+1)
+            candidates.append(i + 1)
             candidates_value.append(sumDiff[i])
 
     if len(candidates) > 20:
@@ -58,20 +61,27 @@ def getKeyFrames(vidcap, store_frame_path):
 
 def getAudioClip(video_reading_path, audio_storing_path):
     clip = mp.VideoFileClip(video_reading_path)
-    clip.audio.write_audiofile(audio_storing_path)
+    try:
+        clip.audio.write_audiofile(audio_storing_path)
+    except Exception:
+        print Exception
 
 def extract_for_folder(input_video_folder, output_frame_folder, output_audio_folder):
     for video_path in glob.glob(input_video_folder + "/*.mp4"):
-        vidcap = cv2.VideoCapture(video_path)
-        video_id = video_path[video_path.rfind("/") + 1:-4]
         
+        video_id = video_path[video_path.rfind("/") + 1:-4]
+        # if int(video_id) < 1001088152326610944:
+        #     continue
+        print video_id
+        vidcap = cv2.VideoCapture(video_path)
+
         frame_out_path = output_frame_folder + video_id + '-'
         getKeyFrames(vidcap=vidcap, store_frame_path=frame_out_path)
         vidcap.release()
 
         audio_out_path = output_audio_folder + video_id + '.wav'
         getAudioClip(video_path, audio_out_path)
-        print video_id
+        
 
 if __name__ == '__main__':
     extract_for_folder(train_video_folder, train_frame_folder, train_audio_folder)
