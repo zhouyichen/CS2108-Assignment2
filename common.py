a753f8596c4d33b98718ad35b5676e572e3a7988
zhouyichen <zhouyichen123@gmail.com>
2016-10-16 19:42:14 +0800

    create common file to be imported

---
+++ common.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+data_folder = 'data/'
+video_folder = data_folder + 'video/'
+train_video_folder = video_folder + 'training/'
+test_video_folder = video_folder + 'validation/'
+
+frame_folder = data_folder + 'frame/'
+train_frame_folder = frame_folder + 'training/'
+test_frame_folder = frame_folder + 'validation/'
+
+audio_folder = data_folder + 'audio/'
+train_audio_folder = audio_folder + 'training/'
+test_audio_folder = audio_folder + 'validation/'
