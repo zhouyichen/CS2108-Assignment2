aabdc853d0affd870b52af0f7e94f472f416214e
zhouyichen <zhouyichen123@gmail.com>
2016-10-17 18:20:35 +0800

    extract visual features

--- common.py
+++ common.py
@@ -18,6 +18,10 @@ acoustic_folder = data_folder + 'acoustic/'
 train_acoustic_folder = acoustic_folder + 'training/'
 test_acoustic_folder = acoustic_folder + 'validation/'
 
+visual_folder = data_folder + 'visual/'
+train_visual_folder = visual_folder + 'training/'
+test_visual_folder = visual_folder + 'validation/'
+
 mfcc_file_name = 'mfcc.csv'
 train_mfcc_path = train_acoustic_folder + mfcc_file_name
 test_mfcc_path = test_acoustic_folder + mfcc_file_name
@@ -41,3 +45,9 @@ test_filenames_path = test_acoustic_folder + filenames_file_name
 textual_folder = data_folder + 'textual/'
 textual_train_file_name = textual_folder + 'vine_desc_training_'
 textual_test_file_name = textual_folder + 'vine_desc_validation_'
+
+dp_train_raw_data = train_visual_folder + 'dp_raw.csv'
+vc_train_raw_data = train_visual_folder + 'vc_raw.csv'
+
+dp_test_raw_data = test_visual_folder + 'dp_raw.csv'
+vc_test_raw_data = test_visual_folder + 'vc_raw.csv'
