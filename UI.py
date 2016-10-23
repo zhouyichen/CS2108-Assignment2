ed193ebefd9344048030cd9fe68421e8ea79eb9c
zhouyichen <zhouyichen123@gmail.com>
2016-10-23 21:21:22 +0800

    properly working UI

--- UI.py
+++ UI.py
@@ -12,6 +12,10 @@ from searchers.visual_searcher import VisualSeacher
 import moviepy.editor as mp
 import os
 
+def combine_results(acoustic_results, visual_results, acoustic_weight=10, visual_weight=1):
+	combined_porb = np.exp(acoustic_results) * acoustic_weight + np.exp(visual_results) * visual_weight
+	return combined_porb.argsort()[-5:][::-1]
+
 class UI_class:
 	def __init__(self, master, audio_storing_path, frame_storing_path):
 		self.audio_storing_path = audio_storing_path
@@ -20,6 +24,7 @@ class UI_class:
 		topframe = Frame(self.master)
 		topframe.pack()
 		
+		self.venues = np.loadtxt('data/venue-name.txt', dtype=str, delimiter=',')
 		acoustic_model = load_data(model_folder+str(64)+acoustic_gmm_models)
 		normalizer = load_data(train_acoustic_normalizer_path)
 		self.acoustic_seacher = AcousticSeacher(acoustic_model, normalizer)
@@ -109,23 +114,33 @@ class UI_class:
 
 		acoustic_results = self.acoustic_seacher.search(self.query_audio_path)
 		visual_results = self.visual_seacher.search(self.query_frames_folder)
-		print acoustic_results
-		print visual_results
+
+		acoustic_weight = 10
+		visual_weight = 1
+
+		top_five = combine_results(acoustic_results, visual_results, 
+			acoustic_weight=acoustic_weight, visual_weight=visual_weight)
+
 		venue_tags = []
+		for v in top_five:
+			venue_tags.append(self.venues[v])
 
-		venue_img = Image.open("venue_background.jpg")
-		draw = ImageDraw.Draw(venue_img)
 
-		font = ImageFont.truetype("/Library/Fonts/Arial.ttf",size=66)
+		for v, venue_text in enumerate(venue_tags):
 
-		draw.text((50,50), venue_text, (0, 0, 0), font=font)
+			venue_img = Image.open("venue_background.jpg")
+			draw = ImageDraw.Draw(venue_img)
 
-		resized = venue_img.resize((100, 100), Image.ANTIALIAS)
-		tkimage =ImageTk.PhotoImage(resized)
+			font = ImageFont.truetype("/Library/Fonts/Arial.ttf",size=45)
 
-		myvar = Label(self.result_img_frame, image=tkimage)
-		myvar.image= tkimage
-		myvar.grid(row=self.lastR, column=self.lastC+1)
+			draw.text((50 - 5*(len(venue_text)-7),50), venue_text, (0, 0, 0), font=font)
+
+			resized = venue_img.resize((100, 100), Image.ANTIALIAS)
+			tkimage =ImageTk.PhotoImage(resized)
+
+			myvar = Label(self.result_img_frame, image=tkimage)
+			myvar.image= tkimage
+			myvar.grid(row=1, column=v)
 
 		self.result_img_frame.mainloop()
 
