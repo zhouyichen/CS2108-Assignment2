2e3ad3ead1dd0eab7ceed28a485b19b95ccbd4a3
zhouyichen <zhouyichen123@gmail.com>
2016-10-23 14:16:25 +0800

    refactor extraction of acoustic features

--- extract_acoustic.py
+++ extract_acoustic.py
@@ -7,61 +7,12 @@ import librosa
 import glob
 from common import *
 import cPickle as pickle
+from searchers.acoustic_searcher import getAcousticFeaturesFromPath
 import csv
 
-def getAcousticFeatures(y, sr, statistics=True):
-	# 4. Compute Melspectrogram features from the raw signal.
-	feature_spect = librosa.feature.melspectrogram(y=y, sr=sr, fmax=50000)
-
-	# 5. Compute MFCC features from the Melspectrogram.
-	mfcc = librosa.feature.mfcc(S=librosa.logamplitude(feature_spect), n_mfcc=20)
-	mfcc_delta = librosa.feature.delta(mfcc)
-	mfcc_delta2 = librosa.feature.delta(mfcc, order=2)
-
-	# 6. Compute Zero-Crossing features from the raw signal.
-	feature_zerocrossing = librosa.feature.zero_crossing_rate(y=y)
-	z_delta = librosa.feature.delta(feature_zerocrossing)
-	z_delta2 = librosa.feature.delta(feature_zerocrossing, order=2)
-
-	# 7. Compute Root-Mean-Square (RMS) Energy for each frame.
-	feature_energy = librosa.feature.rmse(y=y)
-	e_delta = librosa.feature.delta(feature_energy)
-	e_delta2 = librosa.feature.delta(feature_energy, order=2)
-
-	feature_matrix = np.vstack((mfcc, mfcc_delta, mfcc_delta2,
-		feature_zerocrossing, z_delta, z_delta2,
-		feature_energy, e_delta, e_delta2)).T
-	
-	feature_matrix = feature_matrix[1:-1, :]
-	if statistics:
-		return {
-			'feat': feature_matrix,
-			'stat': {
-				'mean': np.mean(feature_matrix, axis=0),
-				'std': np.std(feature_matrix, axis=0),
-				'N': feature_matrix.shape[0],
-				'S1': np.sum(feature_matrix, axis=0),
-				'S2': np.sum(feature_matrix ** 2, axis=0),
-			}
-		}
-	else:
-		return feature_matrix
-
-def getAcousticFeaturesFromPath(audio_reading_path):
-	# 1. Load the audio clip;
-	y, sr = librosa.load(audio_reading_path)
-	# y = extend_to_length_k(y, AUDIO_LENGTH)
-
-	# 2. Separate harmonics and percussives into two waveforms.
-	# y_harmonic, y_percussive = librosa.effects.hpss(y)
-
-	# # 3. Beat track on the percussive signal.
-	# tempo, beat_frames = librosa.beat.beat_track(y=y_percussive, sr=sr)
-
-	return getAcousticFeatures(y, sr)
 
 def extract_for_folder(input_audio_folder, output_folder):
-	acoustic_out_file = open(train_acoustic_combined_path, 'wb')
+	acoustic_out_file = open(output_folder + acoustic_combined_file_name, 'wb')
 	# mfcc_writer = csv.writer(mfcc_out_file)
 
 	# melspectrogram_out_file = open(output_folder + melspectrogram_file_name, 'w')
@@ -89,5 +40,5 @@ def extract_for_folder(input_audio_folder, output_folder):
 	# filenames_writer.writerow(audio_ids)
 
 if __name__ == '__main__':
-	extract_for_folder(train_audio_folder, train_acoustic_folder)
+	# extract_for_folder(train_audio_folder, train_acoustic_folder)
 	extract_for_folder(test_audio_folder, test_acoustic_folder)
