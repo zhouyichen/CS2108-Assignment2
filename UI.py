6b774643293b8ef7ed9130e4adb905d786c163a8
zhouyichen <zhouyichen123@gmail.com>
2016-10-23 18:00:31 +0800

    GUI: extract frames and audio from input video

--- UI.py
+++ UI.py
@@ -9,11 +9,12 @@ import tkMessageBox
 from featureNormalizer import FeatureNormalizer
 from searchers.acoustic_searcher import AcousticSeacher
 from searchers.visual_searcher import VisualSeacher
+import moviepy.editor as mp
 import os
 
 class UI_class:
-	def __init__(self, master, search_path, frame_storing_path):
-		self.search_path = search_path
+	def __init__(self, master, audio_storing_path, frame_storing_path):
+		self.audio_storing_path = audio_storing_path
 		self.master = master
 		self.frame_storing_path = frame_storing_path
 		topframe = Frame(self.master)
@@ -30,6 +31,7 @@ class UI_class:
 		Label(topframe).grid(row=0, columnspan=2)
 		self.bbutton= Button(topframe, text=" Choose an video ", command=self.browse_query_img)
 		self.bbutton.grid(row=1, column=1)
+		
 		self.cbutton = Button(topframe, text=" Estimate its venue ", command=self.show_venue_category)
 		self.cbutton.grid(row=1, column=2)
 		Label(topframe).grid(row=3, columnspan=4)
@@ -40,14 +42,19 @@ class UI_class:
 	def browse_query_img(self):
 
 		self.filename = tkFileDialog.askopenfile(title='Choose an Video File').name
-
-		allframes = os.listdir(self.frame_storing_path)
 		self.videoname = self.filename.strip().split("/")[-1].replace(".mp4","")
+
+		vidcap = cv2.VideoCapture(self.filename)
+		directory = self.frame_storing_path + self.videoname + '/'
+		if not os.path.exists(directory):
+			os.makedirs(directory)
+		self.visual_seacher.extract_frames(vidcap, directory)
+
+		allframes = os.listdir(directory)
 
 		self.frames = []
 		for frame in allframes:
-			if self.videoname +"-frame" in frame:
-				self.frames.append(self.frame_storing_path + frame)
+			self.frames.append(directory + frame)
 
 		COLUMNS = len(self.frames)
 		self.columns = COLUMNS
@@ -55,14 +62,10 @@ class UI_class:
 
 		if hasattr(self, 'query_img_frame'):
 			self.query_img_frame.pack_forget()
-			self.result_img_frame.pack_forget()
+			if hasattr(self, 'result_img_frame'):
+				self.result_img_frame.pack_forget()
 		self.query_img_frame = Frame(self.master)
 		self.query_img_frame.pack()
-
-		if COLUMNS == 0:
-			self.frames.append("none.png")
-			print("Please extract the key frames for the selected video first!!!")
-			COLUMNS = 1
 
 		for frame in self.frames:
 
@@ -80,7 +83,15 @@ class UI_class:
 				self.lastR = r
 				self.lastC = c
 			except Exception, e:
+				print e
 				continue
+
+		clip = mp.VideoFileClip(self.filename)
+		audio_out_path = self.audio_storing_path + self.videoname + '.wav'
+		try:
+			clip.audio.write_audiofile(audio_out_path)
+		except Exception, e:
+			print e
 
 		self.query_img_frame.mainloop()
 
@@ -96,28 +107,28 @@ class UI_class:
 		self.result_img_frame = Frame(self.master)
 		self.result_img_frame.pack()
 
-			# Please note that, you need to write your own classifier to estimate the venue category to show blow.
-			if self.videoname == '1':
-			   venue_text = "Home"
-			elif self.videoname == '2':
-				venue_text = 'Bridge'
-			elif self.videoname == '4':
-				venue_text = 'Park'
+		# Please note that, you need to write your own classifier to estimate the venue category to show blow.
+		if self.videoname == '1':
+		   venue_text = "Home"
+		elif self.videoname == '2':
+			venue_text = 'Bridge'
+		elif self.videoname == '4':
+			venue_text = 'Park'
 
-			venue_img = Image.open("venue_background.jpg")
-			draw = ImageDraw.Draw(venue_img)
+		venue_img = Image.open("venue_background.jpg")
+		draw = ImageDraw.Draw(venue_img)
 
-			font = ImageFont.truetype("/Library/Fonts/Arial.ttf",size=66)
+		font = ImageFont.truetype("/Library/Fonts/Arial.ttf",size=66)
 
-			draw.text((50,50), venue_text, (0, 0, 0), font=font)
+		draw.text((50,50), venue_text, (0, 0, 0), font=font)
 
-			resized = venue_img.resize((100, 100), Image.ANTIALIAS)
-			tkimage =ImageTk.PhotoImage(resized)
+		resized = venue_img.resize((100, 100), Image.ANTIALIAS)
+		tkimage =ImageTk.PhotoImage(resized)
 
-			myvar = Label(self.result_img_frame, image=tkimage)
-			myvar.image= tkimage
-			myvar.grid(row=self.lastR, column=self.lastC+1)
+		myvar = Label(self.result_img_frame, image=tkimage)
+		myvar.image= tkimage
+		myvar.grid(row=self.lastR, column=self.lastC+1)
 
 		self.result_img_frame.mainloop()
 
-window = UI_class(root, search_path='testing/audio/', frame_storing_path='testing/frame/')
+window = UI_class(root, audio_storing_path='testing/audio/', frame_storing_path='testing/frame/')
